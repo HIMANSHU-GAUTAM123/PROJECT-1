89d250457cfbffb3e62fe94b112681ba498a6cf0
HIMANSHU-GAUTAM123 <himanshuofficial123@gmail.com>
2023-07-26 16:11:23 +0530

    initaial

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Home from './pages/Home/Home'
 import './App.css';
 import Navigation from './components/shared/Navigation/Navigation';
 import {BrowserRouter, Routes , Route} from 'react-router-dom';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +11,13 @@ function App() {
       <Routes>
         <Route exact path="/"   element={<Home/>}>
             
+        </Route>
+        <Route exact path="/register"   element={<Register/>}>
+        
+            
+        </Route>
+        <Route exact path="/login"   element={<Login/>}>
+
         </Route>
         
       </Routes>
